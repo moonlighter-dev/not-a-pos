c14703827d4ab4f5b42317bef31f12bd33bc0956
moonlighter-dev <92267486+moonlighter-dev@users.noreply.github.com>
2022-11-09 14:30:04 -0600

    added styling links to html head, moved styling folder, added fs to renderer

--- renderer.js
+++ renderer.js
@@ -1,6 +1,20 @@
 const information = document.getElementById('info')
 const { remote } = require('electron')
 const dbInstance = remote.getGlobal('db')
+const fs = require("fs");
+const { dialog } = require("electron").remote;
+
+// button click event
+document.getElementById("save").addEventListener("click", () => {
+    const data = "Successfully wrote to the desktop"; // the data we want to save to the desktop
+    //launch save dialog window
+    dialog.showSaveDialog(filename => {
+        //save file at the destination indicated by filename
+        fs.writeFileSync(filename + ".txt", data, "utf-8", () => {
+            console.log("attempted to write to the desktop");
+        });
+    });
+});
 
 // supposed to do something with db Instance - need to research a little more
 //https://dev.to/ctxhou/local-data-storage-for-electron-2h4p
